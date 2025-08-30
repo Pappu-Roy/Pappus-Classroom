292cbf4be24a029bbfeaff65a045634e6caa2e62
Pappu <pappuroy10102000@gmail.com>
2025-08-30 18:08:04 +0600

    Project Completed

--- db.sql
+++ db.sql
@@ -46,3 +46,8 @@ CREATE TABLE student_classes (
 
 -- For Initial Admin User
 INSERT INTO `users` (`first_name`, `last_name`, `email`, `password`, `role`) VALUES ('Admin', 'User', 'admin@example.com', '$2y$10$tF.D7sX3bK8.jH7gQ3cQd.kO4hQ4q8.f4x5fC0X.b.L7c0zL4L.e.', 'admin');
+
+
+-- last code for Student Management System Database
+ALTER TABLE classroom_posts ADD COLUMN file_path VARCHAR(255) NULL AFTER post_content;
+ALTER TABLE assignments ADD COLUMN file_path VARCHAR(255) NULL AFTER deadline;
